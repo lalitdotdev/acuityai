304d5695b51d168fcc2849f4dffa801232e1a456
Lalit sharma <geekymonkcp742k@gmail.com>
2024-06-18 13:47:47 +0530

    Implement async function getInlineAnchor using PlasmoGetInlineAnchor with INJECTED_ELEMENT_ID for element selection and 'afterbegin' insert position.

--- src/contents/plasmo.ts
+++ src/contents/plasmo.ts
@@ -1,10 +1,15 @@
-import type { PlasmoCSConfig } from "plasmo"
+import type { PlasmoCSConfig, PlasmoGetInlineAnchor } from "plasmo"
 
 const INJECTED_ELEMENT_ID = "#secondary.style-scope.ytd-watch-flexy"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.youtube.com/*"]
 }
+
+export const getInlineAnchor: PlasmoGetInlineAnchor = async () => ({
+  element: document.querySelector(INJECTED_ELEMENT_ID),
+  insertPosition: "afterbegin"
+})
 
 window.addEventListener("load", () => {
   console.log("content script loaded")
