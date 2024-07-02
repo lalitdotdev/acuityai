271f77cd8c1d29a19fc47b5b36c8b004d70b2fbd
Lalit sharma <geekymonkcp742k@gmail.com>
2024-07-02 16:50:03 +0530

    feat(add transcript provider): Add Providers component to wrap children with ExtensionProvider, TranscriptProvider, and SummaryProvider.

--- components/provider.tsx
+++ components/provider.tsx
@@ -1,9 +1,15 @@
 import { ExtensionProvider } from "@/contexts/extension-context";
+import { SummaryProvider } from "@/contexts/summary-context";
+import { TranscriptProvider } from "@/contexts/transcript-context";
 
 export default function Providers({ children }) {
     return (
         <ExtensionProvider>
-            {children}
+            <TranscriptProvider>
+                <SummaryProvider>
+                    {children}
+                </SummaryProvider>
+            </TranscriptProvider>
         </ExtensionProvider>
     )
 }
