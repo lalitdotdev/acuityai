ab61a9195a12f748ba7aa0a626aa313f2e11d12a
Lalit sharma <geekymonkcp742k@gmail.com>
2024-07-02 12:04:14 +0530

    feat(clean text-transcript): Format transcript events into time-stamped text lines.

--- utils/functions.ts
+++ utils/functions.ts
@@ -92,3 +92,37 @@ export function cleanJsonTranscipt(transcript) {
   // return the chunks array
   return chunks
 }
+
+// function to clean the json transcript and return a plain text version of the transcript with timestamps for each line
+export function cleanTextTranscript(transcript) {
+  let textLines = []
+  let tempText = ""
+  let lastTime = 0
+
+  transcript.events.forEach((event) => {
+    if (event.segs) {
+      event.segs.forEach((seg) => {
+        const segmentStartTimeMs = event.startMs + (seg.tOffsetMs || 0)
+
+        if (
+          tempText &&
+          (segmentStartTimeMs - lastTime > 1000 || seg.utf8 === "\n")
+        ) {
+          const timeFormatted = new Date(lastTime).toISOString().substr(11, 12)
+          textLines.push(`${timeFormatted}: ${tempText.trim()}`)
+          tempText = ""
+        }
+
+        lastTime = segmentStartTimeMs
+        tempText += seg.utf8
+      })
+    }
+  })
+
+  if (tempText) {
+    const timeFormatted = new Date(lastTime).toISOString().substr(11, 12)
+    textLines.push(`${timeFormatted}: ${tempText.trim()}`)
+  }
+
+  return textLines.join("\n")
+}
